213d2b396d42850c5737ac1bfaa51e1171ec5f8a
Ethan Resnick <ethan.resnick@gmail.com>
2015-02-22 15:13:30 -0500

    Default check-in list should only use public events

--- controllers/checkin.js
+++ controllers/checkin.js
@@ -4,7 +4,7 @@ var API_BASE_URL = "https://api.tnyu.org/v1.0/";
 module.exports = {
   showEvents: function(req, res, next, urlFor) {
     request({
-      url: API_BASE_URL + "events/check-in-eligible",
+      url: API_BASE_URL + "events/public-check-in-eligible",
       json: true,
       rejectUnauthorized: false
     }, function(err, response, body) {
@@ -32,6 +32,8 @@ module.exports = {
 
   configure: function(req, res, next, urlFor) {
     request({
+      // include non-public events so board members can 
+      // create check in pages, e.g., for our eboard meetings.
       url: "https://api.tnyu.org/v1.0/events/next-10",
       json: true,
       rejectUnauthorized: false
