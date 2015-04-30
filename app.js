3a536b3254e9ac93c96048545d1961286f0ad989
Abhi Agarwal <me@abhiagarwal.com>
2015-04-29 20:52:42 -0400

    Adding jscsrc, jshintrc, and fixing code

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var express     = require('express')
-  , path        = require('path')
-  , favicon     = require('serve-favicon')
-  , logger      = require('morgan')
-  , bodyParser  = require('body-parser')
-  , debug       = require('debug')('signin')
-  , router      = require('express-simple-router')
-  , checkinController = require('./controllers/checkin');
+'use strict';
 
-var app = express(), port = process.env.PORT || 3000;
+var express     = require('express');
+var path        = require('path');
+var favicon     = require('serve-favicon');
+var logger      = require('morgan');
+var bodyParser  = require('body-parser');
+var debug       = require('debug')('signin');
+var router      = require('express-simple-router');
+var checkinController = require('./controllers/checkin');
+
+var app = express();
+var port = process.env.PORT || 3000;
 
 // config
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', port);
-app.set('host', (app.get('env') === 'development' ? 'lvh.me:'+ port : 'checkin.techatnyu.org'));
+app.set('host', (app.get('env') === 'development' ? 'lvh.me:' + port : 'checkin.techatnyu.org'));
 
 //static stuff
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,29 +30,29 @@ app.use(checkinRouter.handle({'Checkin': checkinController}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + port);
+	debug('Express server listening on port ' + port);
 });
--- controllers/checkin.js
+++ controllers/checkin.js
@@ -1,63 +1,65 @@
+'use strict';
+
 var request = require('request-promise');
-var API_BASE_URL = "https://api.tnyu.org/v1.0/";
+var API_BASE_URL = 'https://api.tnyu.org/v1.0/';
 
 module.exports = {
-  showEvents: function(req, res, next, urlFor) {
-    request({
-      url: API_BASE_URL + "events/public-check-in-eligible",
-      json: true,
-      rejectUnauthorized: false
-    }, function(err, response, body) {
-      res.render('showEvents', {events: body.events});
-    });
-  },
+	showEvents: function(req, res, next, urlFor) {
+		request({
+			url: API_BASE_URL + "events/public-check-in-eligible",
+			json: true,
+			rejectUnauthorized: false
+		}, function(err, response, body) {
+			res.render('showEvents', {events: body.events});
+		});
+	},
 
-  showConfiguredEvent: function(req, res, next, urlFor) {
-    request({
-      url: API_BASE_URL + "events/" + req.params.event_id,
-      json: true,
-      rejectUnauthorized: false
-    }, function(err, response, body) {
-      res.render('showEvents', {events: body.events, fromConfig: true});
-    });
-  },
+	showConfiguredEvent: function(req, res, next, urlFor) {
+		request({
+			url: API_BASE_URL + 'events/' + req.params.event_id,
+			json: true,
+			rejectUnauthorized: false
+		}, function(err, response, body) {
+			res.render('showEvents', {events: body.events, fromConfig: true});
+		});
+	},
 
-  checkin: function(req, res, next, urlFor) {
-    res.render('checkin');
-  },
+	checkin: function(req, res, next, urlFor) {
+		res.render('checkin');
+	},
 
-  thanks: function(req, res, next, urlFor) {
-    res.render('thanks');
-  },
+	thanks: function(req, res, next, urlFor) {
+		res.render('thanks');
+	},
 
-  configure: function(req, res, next, urlFor) {
-    request({
-      url: "https://api.tnyu.org/v1.0/events/next-10-publicly",
-      json: true,
-      rejectUnauthorized: false
-    }, function(err, response, body) {
-      var events = body.events instanceof Array ? body.events : [body.events];
+	configure: function(req, res, next, urlFor) {
+		request({
+			url: 'https://api.tnyu.org/v1.0/events/next-10-publicly',
+			json: true,
+			rejectUnauthorized: false
+		}, function(err, response, body) {
+			var events = body.events instanceof Array ? body.events : [body.events];
 
-      events.forEach(function(event) {
-        event.checkInUrl = urlFor("showConfiguredEvent", {absolute: true, params: {event_id: event.id}});
-      })
+			events.forEach(function(event) {
+				event.checkInUrl = urlFor('showConfiguredEvent', {absolute: true, params: {event_id: event.id}});
+			});
 
-      res.render('configure', {events: events});
-    });
-  },
+			res.render('configure', {events: events});
+		});
+	},
 
-  number: function(req, res, next, urlFor) {
-    request({
-      url: API_BASE_URL + "events/" + req.params.event_id,
-      json: true,
-      rejectUnauthorized: false
-    }, function(err, response, body) {
-      var events = body.events instanceof Array ? body.events : [body.events];
-      var attendeeNumber = 0;
-      if (events[0] && events[0].links && events[0].links.attendees){
-        attendeeNumber = events[0].links.attendees.ids.length;
-      }
-      res.render('number', {data : {'attendees' : attendeeNumber}});
-    });
-  },
-}
+	number: function(req, res, next, urlFor) {
+		request({
+			url: API_BASE_URL + 'events/' + req.params.event_id,
+			json: true,
+			rejectUnauthorized: false
+		}, function(err, response, body) {
+			var events = body.events instanceof Array ? body.events : [body.events];
+			var attendeeNumber = 0;
+			if (events[0] && events[0].links && events[0].links.attendees) {
+				attendeeNumber = events[0].links.attendees.ids.length;
+			}
+			res.render('number', {data : {'attendees' : attendeeNumber}});
+		});
+	},
+};
