b48289bbcb006d5d2e0dcd222fb23f9227c1f04c
Abhi Agarwal <me@abhiagarwal.com>
2015-09-19 22:07:37 -0400

    Adding sort improvement, and switching from tables to duplicating row

--- app/js/controllers/events.js
+++ app/js/controllers/events.js
@@ -3,7 +3,7 @@
 angular
 .module('app.controllers')
 .controller('EventsCtrl', function($scope, Restangular) {
-	$scope.loadingPromise = Restangular.one('events/upcoming-publicly?page[limit]=10')
+	$scope.loadingPromise = Restangular.one('events/upcoming-publicly?page[limit]=10&sort=%2bstartDateTime')
 		.get()
 		.then(function(data) {
 			$scope.events = data;
