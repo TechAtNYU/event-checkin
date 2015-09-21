68910143032e37c191fb656f36da6ca5ff2835ac
Abhi Agarwal <me@abhiagarwal.com>
2015-09-21 18:08:25 -0400

    Update events.js

--- app/js/controllers/events.js
+++ app/js/controllers/events.js
@@ -3,7 +3,7 @@
 angular
 .module('app.controllers')
 .controller('EventsCtrl', function($scope, Restangular) {
-	$scope.loadingPromise = Restangular.one('events/upcoming-publicly?page[limit]=10&sort=%2bstartDateTime')
+	$scope.loadingPromise = Restangular.one('events/public-check-in-eligible?page[limit]=10&sort=%2bstartDateTime')
 		.get()
 		.then(function(data) {
 			$scope.events = data;
