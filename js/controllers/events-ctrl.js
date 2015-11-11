c742e1df98eb33644604721c74f779374d78cb74
Abhi Agarwal <me@abhiagarwal.com>
2015-11-11 11:53:42 -0500

    Fixing indentation
    Fixing identation
    
    Fixing identation

--- js/controllers/events-ctrl.js
+++ js/controllers/events-ctrl.js
@@ -3,18 +3,18 @@
 angular
 .module('app.controllers')
 .controller('EventsCtrl', function($scope, Restangular) {
-    $scope.isTeamMember = false;
-    Restangular.one('people/me')
-        .get()
-        .then(function(data) {
-            if (data &&data.attributes && data.attributes.roles && data.attributes.roles.length > 0) {
-                $scope.isTeamMember = true;
-            }
-        })
-        .catch(function(error){
-            $scope.isTeamMember = false;
-            console.log(error);
-        });
+	$scope.isTeamMember = false;
+	Restangular.one('people/me')
+		.get()
+		.then(function(data) {
+			if (data &&data.attributes && data.attributes.roles && data.attributes.roles.length > 0) {
+				$scope.isTeamMember = true;
+			}
+		})
+		.catch(function(error){
+			$scope.isTeamMember = false;
+			console.log(error);
+		});
 
 	$scope.loadingPromise = Restangular.one('events/public-check-in-eligible?page[limit]=10&sort=%2bstartDateTime')
 		.get()
