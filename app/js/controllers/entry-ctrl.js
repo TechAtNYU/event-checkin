76de0398941434304f950c0e7f77ab2155a06f13
Ashley Teoh <atq203@nyu.edu>
2018-02-05 00:46:01 -0500

    q#

--- app/js/controllers/entry-ctrl.js
+++ app/js/controllers/entry-ctrl.js
@@ -58,19 +58,14 @@ angular
 
 		$scope.addToMailingList = function(person) {
 			var access_token = 'f234e56583e242b25e3f76a7fe4e4789ab98ed0c';
-			var mailtrain_url = 'http://mailtrain.tnyu.org/api/subscribe/HJNc8AuSz'
+			var mailtrain_url = 'http://mailtrain.tnyu.org/api/subscribe/HJNc8AuSz';
 
 			$http({
-				method  : 'POST',
+				method: 'POST',
 				crossDomain: true,
-				url     : mailtrain_url + '?access_token=' + access_token,
-				data:
-					{
-						'EMAIL': person.attributes.contact.email,
-						'REQUIRE_CONFIRMATION': 'yes'
-					}
-			})
-			.success(function(response) {
+				url: mailtrain_url + '?access_token=' + access_token,
+				data: { 'EMAIL': person.attributes.contact.email, 'REQUIRE_CONFIRMATION': 'yes' }
+			}).success(function(response) {
 				alert('success! – ' + response.data.id)
 			})
 		};
